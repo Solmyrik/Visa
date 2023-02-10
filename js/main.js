3d6954a4e14b758594a46f5cb2936c8c4fe63631
Dmitry <103439106+Solmyrik@users.noreply.github.com>
2023-02-10 20:19:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const iconMenu = document.querySelector('.header__burger');
+if (iconMenu) {
+  const menuBody = document.querySelector('.menu__body');
+  iconMenu.addEventListener('click', function (e) {
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
+
+function ibg() {
+  let ibg = document.querySelectorAll('.ibg');
+  for (let i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+  }
+}
+ibg();
